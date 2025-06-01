923c7203cc8fe32f94656f189f6d11752c3dfd8c
Rochelle Awuor <awuorochelle75@gmail.com>
2025-06-01 15:07:01 +0300

    adds new files

---
+++ app/migrations/versions/e0356aa46e4a_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e0356aa46e4a
+Revises: 
+Create Date: 2025-06-01 15:00:49.849365
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e0356aa46e4a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
